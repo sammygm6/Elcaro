54efa7d3e6059f074c197ebd13daec3be5f09c07
SammyGuevara <sammygm6@gmail.com>
2016-08-27 00:43:14 -0600

    termine haciendo un availList para los borrados

--- src/elcaro/ARLF.java
+++ src/elcaro/ARLF.java
@@ -1,9 +1,8 @@
 package elcaro;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class ARLF {
 
@@ -11,12 +10,27 @@ public class ARLF {
     private int size;
     private File archivo;
     private boolean llave;
+    private RandomAccessFile RAF;
+    private int address;
+    private Stack<String> AvailList;
 
     public ARLF() {
     }
 
     public ARLF(int size) {
         this.size = size;
+    }
+
+    public RandomAccessFile getRAF() {
+        return RAF;
+    }
+
+    public void setRAF(RandomAccessFile RAF) {
+        this.RAF = RAF;
+    }
+
+    public int getAddress(int RecordNumber) {
+        return RecordNumber * size + 1;
     }
 
     public ARLF(File registro) {
@@ -29,7 +43,7 @@ public class ARLF {
 
     public void setData(String data) {
         this.data = data;
-        
+
     }
 
     public int getSize() {
@@ -69,6 +83,31 @@ public class ARLF {
             FileWriter writer = new FileWriter(archivo.getAbsolutePath(), true);
             writer.write(datos);
             writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isUpperCase(String Temp) {
+        String Temp2 = Temp;
+        if (Temp.equals(Temp2.toUpperCase())) {
+            return true;
+        }
+        return false;
+    }
+
+    public void BuildAvailList() {
+        this.AvailList = new Stack();
+            try {
+            this.RAF = new RandomAccessFile(this.archivo,"rw");
+            RAF.seek(1);
+            int avail = RAF.read();
+                while(avail != -1){
+                    AvailList.push(Integer.toString(avail));
+                    RAF.seek(avail);
+                    avail = RAF.read();
+                }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
